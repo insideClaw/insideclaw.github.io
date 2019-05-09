da77bdf7d12f538f9a4752cbe3686a1f830eb5d6
insideClaw <insideClawy@gmail.com>
2019-05-09 16:32:48 +0100

    Implement combining of string-based seeds

--- fairaffle/raffle.js
+++ fairaffle/raffle.js
@@ -21,12 +21,24 @@ function getSaltySeededRandom(seed, max){
   return(seededRandom)
 }
 
-function combineSeeds(seed1, seed2){
-  // Create a unique signature (collate multiple seeds), given two arbitrary strings.
-  // TODO: Allow strings as seed by converting them to floats later
-  saltedSeed = seed1
-  // TODO: Add the salt from news article once ready
-  return(saltedSeed)
+function representCharactersAsNumbers(inputString){
+  // Loop through every char of the seed and assign its numerical representation into the combined seed
+  numerical_representation = ""
+  for (var i = 0; i < inputString.length; i++) {
+    numerical_representation += inputString.charCodeAt(i)
+  }
+  return(numerical_representation)
+}
+
+function combineSeeds(){
+  // Create a unique signature (collating multiple seeds), given two arbitrary strings.
+  combinedSeed = ""
+  for(var seed in arguments){
+    combinedSeed += representCharactersAsNumbers(seed)
+  }
+  console.log(combinedSeed)
+
+  return(combinedSeed)
 }
 
 async function getNewsArticle(){
@@ -57,13 +69,15 @@ function drawRaffle(){
     // TODO: Select a meaningful item as the chosen one
     seed_secret = newsItems.response.results[0]["id"]
 
+    // Show the fetched secret seed to the user
+    document.getElementById("news").innerHTML = seed_secret;
+
     // TODO: Currently just returns argument1
     final_seed = combineSeeds(seed_reference, seed_secret)
 
-    // End part - reached only if the secret seed has been released
     // Calculate winner
     winner = getSaltySeededRandom(final_seed, number_participants)
     // Present result to the user
-    document.getElementById("result").innerHTML = seed_secret //winner;
+    document.getElementById("result").innerHTML = winner;
   })
 }
